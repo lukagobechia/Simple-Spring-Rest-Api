d2ff8d724ce9c71a6996cbe02175238ed65426ff
unknown <IBSU_Student@303-47.ibsu.edu.ge>
2023-12-12 14:37:32 +0400

    #

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Simple-Spring-Rest-Api
+# Simple Spring Rest Api   
---
+++ src/main/java/ge/ibsu/demo/controllers/TestController.java
@@ -0,0 +1,21 @@
+package ge.ibsu.demo.controllers;
+import ge.ibsu.demo.dto.TestPerson;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = {"application/json"})
+    public String hello(){
+        return  "Hello, world!";
+    }
+    @RequestMapping(value = "/person", method = RequestMethod.POST, produces = {"application/json"})
+    public TestPerson testPerson(@RequestBody TestPerson testPerson){
+        testPerson.setFirstName(testPerson.getFirstName() + " from server");
+        return testPerson;
+    }
+}
